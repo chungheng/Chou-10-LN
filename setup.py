9198a401a32ca8b4f3e5f14e6777581f9bb0dc48
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2015-11-12 17:11:19 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,71 @@
+mport sys, os
+from glob import glob
+
+data_dir = 'data'
+data_files = [(data_dir, [f for f in glob(os.path.join(data_dir, '*'))])]
+
+# Install setuptools if it isn't available:
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+
+from distutils.command.install import INSTALL_SCHEMES
+from distutils.command.install_headers import install_headers
+from setuptools import find_packages
+from setuptools import setup
+
+NAME =               'chou10'
+VERSION =            '0.1'
+AUTHOR =             'Chung-Heng (Jason) Yeh'
+AUTHOR_EMAIL =       'chungheng.yeh@gmail.com'
+URL =                'https://github.com/chungheng/chou-10-LN'
+MAINTAINER =         AUTHOR
+MAINTAINER_EMAIL =   AUTHOR_EMAIL
+DESCRIPTION =        ''
+LONG_DESCRIPTION =   DESCRIPTION
+DOWNLOAD_URL =       URL
+LICENSE =            'BSD'
+CLASSIFIERS = [
+	'Development Status :: 3 - Alpha',
+	'Intended Audience :: Developers',
+	'Intended Audience :: Science/Research',
+	'License :: OSI Approved :: BSD License',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Topic :: Scientific/Engineering',
+	'Topic :: Software Development']
+NAMESPACE_PACKAGES = ['chou10']
+PACKAGES =           find_packages()
+
+if __name__ == "__main__":
+	if os.path.exists('MANIFEST'):
+		os.remove('MANIFEST')
+
+	# This enables the installation of flyor/__init__.py as a data
+	# file:
+	for scheme in INSTALL_SCHEMES.values():
+		scheme['data'] = scheme['purelib']
+
+	setup(
+		name = NAME,
+		version = VERSION,
+		author = AUTHOR,
+		author_email = AUTHOR_EMAIL,
+		license = LICENSE,
+		classifiers = CLASSIFIERS,
+		description = DESCRIPTION,
+		long_description = LONG_DESCRIPTION,
+		url = URL,
+		maintainer = MAINTAINER,
+		maintainer_email = MAINTAINER_EMAIL,
+		namespace_packages = NAMESPACE_PACKAGES,
+		packages = PACKAGES,
+
+		# Force installation of __init__.py in namespace package:
+		data_files = data_files,
+		include_package_data = True,
+		install_requires = [
+			'pandas >= 0.15.0']
+	)
